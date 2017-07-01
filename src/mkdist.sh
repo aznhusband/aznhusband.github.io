df1cb3491778b2367e2c632e84a5bc13bdb0a083
Asian Husband <aznhusband@mail.com>
2017-07-01 07:38:16 -0500

    Add repo code, update mkdist

--- src/mkdist.sh
+++ src/mkdist.sh
@@ -4,3 +4,6 @@ eval `grep "plugin.video.icdrama" plugin.video.icdrama/addon.xml | awk '{print $
 echo $version
 
 find plugin.video.icdrama -type f -print | grep -v ".git" | zip -@ plugin.video.icdrama-$version.zip
+
+eval `grep "repository.aznhusband-kodi-repo" repository.aznhusband-kodi-repo/addon.xml | awk '{print $3}'`
+find repository.aznhusband-kodi-repo -type f -print | grep -v ".git" | zip -@ repository.aznhusband-kodi-repo-$version.zip
