fba6debebfa1744b374044871323e9b32cd9fd62
aznhusband <aznhusband@mail.com>
2017-07-06 14:09:16 -0500

    That was the trick

--- src/kodidirlist.py
+++ src/kodidirlist.py
@@ -2,18 +2,11 @@
 
 import os
 
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['','K','M','G','T','P','E','Z']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
-
 def print_sizes(root):
     for name in sorted(os.listdir(root)):
         if name == 'index.html':
             continue
-        print '<a href="%s">%s</a><td>%d</td>' % (name, name, os.stat(name).st_size)
+        print '<a href="%s">%s</a><td>%dB</td>' % (name, name, os.stat(name).st_size)
 
 
 print_sizes('.')
