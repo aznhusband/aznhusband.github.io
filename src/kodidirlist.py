17c1fd0720adae711553e457aaf3cfba3f9d02b6
aznhusband <aznhusband@mail.com>
2017-07-06 14:02:19 -0500

    Make nice file sizes

--- src/kodidirlist.py
+++ src/kodidirlist.py
@@ -2,9 +2,18 @@
 
 import os
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
 def print_sizes(root):
     for name in sorted(os.listdir(root)):
-        print '<a href="%s">%s</a><td>%d.0B</td>' % (name, name, os.stat(name).st_size)
+        if name == 'index.html':
+            continue
+        print '<a href="%s">%s</a><td>%s</td>' % (name, name, sizeof_fmt(os.stat(name).st_size))
 
 
 print_sizes('.')
