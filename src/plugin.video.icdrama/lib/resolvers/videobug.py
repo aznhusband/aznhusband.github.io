c23b70f92496d3f591e935146276523c2c3969ee
Asian Husband <aznhusband@mail.com>
2018-04-04 21:18:17 -0500

    Hopefully fix for Kodi 16 users

--- src/plugin.video.icdrama/lib/resolvers/videobug.py
+++ src/plugin.video.icdrama/lib/resolvers/videobug.py
@@ -30,7 +30,7 @@ class Videobug(UrlResolver):
         #Videobug changed their video links, trying to get the raw link back. - mugol
         wrapped_url = response.url
         unwrapped_url = urlparse(wrapped_url).query
-        unwrapped_url = unwrapped_url.replace("http", "https")
+        # unwrapped_url = unwrapped_url.replace("http", "https")
         unwrapped_response = requests.get(unwrapped_url, headers=headers)
 
         streams = self._extract_streams(unwrapped_response)
