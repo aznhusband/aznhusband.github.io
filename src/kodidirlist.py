8b4cbc3bce6048c86a8ae523523b0fe3f56ef4a9
Scott Balneaves <sbalneav@beaconia.ca>
2020-06-07 21:55:19 -0500

    Re-add the .zip dir list.  People who aren't properly pulling in the update probably haven't got the repo properly installed.

--- src/kodidirlist.py
+++ src/kodidirlist.py
@@ -6,7 +6,13 @@ def print_sizes(root):
     for name in sorted(os.listdir(root)):
         if name == 'index.html':
             continue
-        print '<a href="%s">%s</a><td>%dB</td>' % (name, name, os.stat(name).st_size)
+        print '<tr><td><a href="%s">%s</a></td><td>%dB</td></tr>' % (name, name, os.stat(name).st_size)
 
 
+print '<html>'
+print '<body>'
+print '<table>'
 print_sizes('.')
+print '</table>'
+print '</body>'
+print '</html>'
